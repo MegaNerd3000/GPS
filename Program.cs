28397896d18edd1b53eba790537055479df6ccce
MegaNerd3000 <142379325+MegaNerd3000@users.noreply.github.com>
2023-08-18 09:02:44 +0200

    sorting
    sorting

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Bubba!");
+            using (var reader = new StreamReader(@"C:\Users\valen\Documents\HTX 3\Programmerign\flyvning1.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    //Console.WriteLine(reader.ReadLine());
+                    var line = reader.ReadLine();
+                    string[] values = line.Split(';');
+                    int i = values.Length;
+                    for(int j = 0; j < i; j++)
+                    {
+                        if(j ==2 || j==3)
+                        {
+                            Console.WriteLine(values[j]);
+                        }
+                        //Console.WriteLine(values[j]);
+                    }
+                }
+            }
         }
     }
 }
